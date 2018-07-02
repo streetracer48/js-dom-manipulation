1818fbedd9e56962370cb72b800929b59273a18a
wasifmiraz <wasifmiraz@mail.com>
2018-07-02 12:25:13 +0600

    query selector and all

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,24 @@
-// import '../styles/index.scss'
+// // import '../styles/index.scss'
 
-import { sum, sub  } from './math'
+// import { sum, sub  } from './math'
 
 
-let headers=document.getElementsByTagName('h1');
-console.log(headers);
+// let headers=document.getElementsByTagName('h1');
+// console.log(headers);
 
-let classes=document.getElementsByClassName('my-para')
-console.log(classes);
-let id= document.getElementById('h1');
-console.log(id);
-let btn = document.getElementById('click-me');
+// let classes=document.getElementsByClassName('my-para')
+// console.log(classes);
+// let id= document.getElementById('h1');
+// console.log(id);
+let btn =$('#click-me');
 let colors = ['red', 'green','yellow', 'blue']
 let i=0;
 
 btn.addEventListener('click', function()
 {
-    let h1 = document.getElementById('my-h1');
-h1.innerHTML="new welcome Dom hehehe :/";
-h1.style.background = colors[i];
+    let h1 = $('#my-h1');
+    h1.innerHTML="new welcome Dom hehehe :/";
+    h1.style.background = colors[i];
 if( i>= colors.length)
 {
     i=0
@@ -31,3 +31,15 @@ i++
 // alert("i am not working hehehe :/");
 
 })
+
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+
+
+let container = document.querySelectorAll('.container')
+console.log(container)
+
+
+
