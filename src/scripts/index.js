bb74a445340510e8975b3c889bc66f9ea661ff4a
wasifmiraz <wasifmiraz@mail.com>
2018-07-02 14:13:29 +0600

    style document with assign

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,36 +10,68 @@
 // console.log(classes);
 // let id= document.getElementById('h1');
 // console.log(id);
-let btn =$('#click-me');
-let colors = ['red', 'green','yellow', 'blue']
-let i=0;
+// let btn =$('#click-me');
+// console.log(btn.style);
+// let colors = ['red', 'green','yellow', 'blue']
+// let i=0;
 
-btn.addEventListener('click', function()
-{
-    let h1 = $('#my-h1');
-    h1.innerHTML="new welcome Dom hehehe :/";
-    h1.style.background = colors[i];
-if( i>= colors.length)
-{
-    i=0
-}
-else {
-i++
+// btn.addEventListener('click', function()
+// {
+//     let h1 = $('#my-h1');
+//     h1.innerHTML="new welcome Dom hehehe :/";
+//     h1.style.background = colors[i];
+// if( i>= colors.length)
+// {
+//     i=0
+// }
+// else {
+// i++
 
-}
+// }
 
 // alert("i am not working hehehe :/");
 
-})
+// })
 
-function $(selector) {
-    return document.querySelector(selector);
+// function $(selector) {
+//     return document.querySelector(selector);
+// }
+
+
+
+// let container = document.querySelectorAll('.container')
+// console.log(container)
+
+let h1 =$('#my-h1')
+
+// h1.style.background='salmon'
+// h1.style.fontSize='26px'
+// h1.style.fontFamiliy='Arial'
+
+let h1style = {
+   background:'salmon',
+fontFamiliy:'Arial',
+fontSize:'26',
+padding:'20px',
+textAlign:'center'
 }
 
+function $(selector){
+   return document.querySelector(selector)
+}
 
+Object.assign(h1.style, h1style)
 
-let container = document.querySelectorAll('.container')
-console.log(container)
+let p =$('#my-para')
 
+let pstyle = {
+    background:'green',
+    fontFamiliy:'Arial',
+    fontSize:'14',
+    padding:'20px',
+    textAlign:'center'
+}
+
+Object.assign(p.style, pstyle);
 
 
