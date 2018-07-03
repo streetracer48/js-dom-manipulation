ac63f135c74a4585b67adaaa7ea8736381fbeaad
wasifmiraz <wasifmiraz@mail.com>
2018-07-03 12:37:25 +0600

    create & deleting DOM Element

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,47 @@
-let btn = document.querySelector('#btns')
+// let root = document.querySelector('#root')
 
-let para= document.querySelector('#para')
+// let jumbotrondiv = document.createElement("div")
 
-let isShown = false
+// jumbotrondiv.className='jumbotron text-center'
+// jumbotrondiv.setAttribute('id','my-jmb')
 
-btn.addEventListener('click', function() {
+// let h1 = document.createElement("h1")
+// h1.className ='display-4 py-5'
+// h1.innerHTML='<strong>THis is DOM</strong>'
+// jumbotrondiv.appendChild(h1)
 
- if(isShown)
- {
-    para.style.visibility = 'hidden'
-    isShown = false
-    btn.innerHTML='show'
+// root.appendChild(jumbotrondiv
 
+let nameField = document.querySelector('#nameField')
+let ul = document.querySelector('#nameList')
 
- }
- else
- {
-     para.style.visibility = 'visible'
-isShown =true
-btn.innerHTML='hide'
-
- }
-
+nameField.addEventListener('keypress', function(event) {
+    if(event.keyCode === 13)
+    {
+        let name = event.target.value
+        createLi(ul,name)
+        event.target.value = ""
+    }
 })
+
+function createLi(ul,name) {
+    let li = document.createElement('li')
+       li.className='list-group-item d-flex'
+       li.innerHTML = name
+       let span = document.createElement('span')
+
+       span.className='ml-auto'
+       span.innerHTML='x'
+       span.fontSize="30px"
+       span.style.color ='red'
+       span.style.cursor='pointer'
+
+       span.addEventListener('click', function() {
+ul.removeChild(li)
+       })
+
+li.appendChild(span)
+ul.appendChild(li);
+
+}
 
